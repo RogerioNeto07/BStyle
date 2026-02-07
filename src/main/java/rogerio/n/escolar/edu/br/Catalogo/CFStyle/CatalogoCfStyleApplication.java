574e76a0ce22810dfab177487f49eea9419d51aa
RogerioNeto07 <joserogeriofonsecauzl@gmail.com>
2026-02-07 15:12:59 -0300

    feat: add authorize button to swagger

--- src/main/java/rogerio/n/escolar/edu/br/Catalogo/CFStyle/CatalogoCfStyleApplication.java
+++ src/main/java/rogerio/n/escolar/edu/br/Catalogo/CFStyle/CatalogoCfStyleApplication.java
@@ -1,13 +1,25 @@
 package rogerio.n.escolar.edu.br.Catalogo.CFStyle;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+    info = @Info(title = "CF Style API", version = "1.0", description = "Catálogo de Produtos e Vendas"),
+    security = @SecurityRequirement(name = "basicAuth")
+)
+@SecurityScheme(
+    name = "basicAuth",
+    type = SecuritySchemeType.HTTP,
+    scheme = "basic"
+)
 public class CatalogoCfStyleApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(CatalogoCfStyleApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(CatalogoCfStyleApplication.class, args);
+    }
 }
